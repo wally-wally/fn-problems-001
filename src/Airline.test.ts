d5cf2d724d2a409661490f11f89022c3414f82cc
kwonmory <khj@gabia.com>
2022-12-13 12:43:12 +0900

    add problems

---
+++ src/Airline.test.ts
@@ -0,0 +1,76 @@
+jest.useFakeTimers();
+import Airline from "./Airline";
+
+/**
+ * 간단한 비행기 탑승 시뮬레이션입니다.
+ * 비행기 탑승은 1명씩 체크인이 가능합니다.
+ * 체크인은 출국 심사관에 따라서 랜덤한 시간을 가집니다.
+ * 현재 여러 타입라인으로 구성되어있습니다.
+ */
+
+describe("Airline", () => {
+    it("첫번째 문제", () => {
+        /**
+         * 타임라인의 순서를 보장해주세요.
+         */
+
+        const gabiaAirline = new Airline();
+
+        gabiaAirline.addToLine("Lime1");
+        gabiaAirline.addToLine("Ellie2");
+        gabiaAirline.addToLine("Wally3");
+        gabiaAirline.addToLine("Mory4");
+
+        jest.runAllTimers();
+
+        expect(gabiaAirline.customersOnBoard).toStrictEqual([
+            "Lime1",
+            "Ellie2",
+            "Wally3",
+            "Mory4"
+        ]);
+    });
+
+    it("두번째 문제", () => {
+      /**
+       * 비행기 사정으로 인해서 선착순으로 도착한 2명만 탑승이 가능합니다.
+       */
+
+      const gabiaAirline = new Airline();
+
+      gabiaAirline.addToLine("Lime1");
+      gabiaAirline.addToLine("Ellie2");
+      gabiaAirline.addToLine("Wally3");
+      gabiaAirline.addToLine("Mory4");
+
+        jest.runAllTimers();
+
+        expect(gabiaAirline.customersOnBoard).toStrictEqual([
+        "Lime1",
+        "Ellie2"
+      ]);
+    });
+
+    it("세번째 문제", () => {
+      /**
+       * 비행사에서 고객이 탑승할 때마다 로그를 가지려고합니다.
+       * 탑승고객을 콘솔로 찍어주세요.
+       * log-{탑승고객명} 으로 출력해주세요.
+       * addCustomerOnBoard 에서 Log를 찍어야합니다.
+       */
+      const consoleWarnMock = jest.spyOn(console, "log").mockImplementation();
+
+      const gabiaAirline = new Airline();
+
+      gabiaAirline.addToLine("Lime1");
+      gabiaAirline.addToLine("Ellie2");
+
+      jest.runAllTimers();
+
+      expect(consoleWarnMock.mock.calls).toEqual(
+        expect.arrayContaining([["log-Lime1"], ["log-Ellie2"]])
+      );
+
+      consoleWarnMock.mockRestore();
+    });
+});
---
+++ src/Airline.ts
@@ -0,0 +1,58 @@
+export default class Airline {
+  private readonly boardingWaitingLine: string[]; // 탑승 대기 줄
+  private readonly _customersOnBoard: string[]; // 탑습 고객
+
+  constructor() {
+    this.boardingWaitingLine = []; // 탑승 대기 줄 초기화
+    this._customersOnBoard = []; // 탑승 고객 초기화
+  }
+
+  // 탑승한 고객 가져오기
+  get customersOnBoard() {
+    return this._customersOnBoard;
+  }
+
+  // 탑승 대기 줄에 줄 서기
+  addToLine(peopleId: string) {
+    console.log(`${peopleId} - 줄서기를 했습니다.`);
+    this.boardingWaitingLine.push(peopleId); // 줋서기
+
+    // 체크인 하기
+    this.checkIn((peopleId) => {
+      console.log(`${peopleId} - 탑승이 완료되었습니다.`);
+      this.addCustomerOnBoard(peopleId); // 탑승 완료 시키기
+    });
+  }
+
+  // 탑승한 인원으로 추가하기
+  addCustomerOnBoard(peopleId: string) {
+    setTimeout(() => {
+      this._customersOnBoard.push(peopleId);
+    }, 0);
+  }
+
+  // 출국 심사를 진행합니다.
+  // 출국 심사는 출국심사원에 따라서 랜덤한 시간을 가집니다.
+  immigration(callback: Function) {
+    setTimeout(() => {
+      console.log("출국 심사 중...");
+      callback();
+    }, Math.floor(Math.random() * 3 * 1000));
+  }
+
+  // 체크인 하기
+  // 체크인은 시간이 걸립니다.
+  checkIn(done: (peopleId: string) => void) {
+    if (!this.boardingWaitingLine.length) {
+      console.log("탑승 가능한 인원이 없습니다.");
+      return;
+    }
+
+    const peopleId = this.boardingWaitingLine.shift(); // 대기줄에서 첫번째 사람을 탑승자로 지정한다.
+
+    this.immigration(() => {
+      if (peopleId)
+        done(peopleId);
+    });
+  }
+}
